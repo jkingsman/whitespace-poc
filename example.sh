768d09eb6ce1ec24cfea0c6ca8a990033e903203
Jack Kingsman <jack.kingsman@gmail.com>
2024-09-24 21:08:11 -0700

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 echo "hello, doing normal things"
-
                                                                                                                                                                                                                                                                                                                                                                              echo "betcha didn't spot me!"
 exit 0
